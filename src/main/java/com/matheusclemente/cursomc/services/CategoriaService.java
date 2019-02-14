d1b9ec133a5da597c3cc69cb8326a3d3abc4041c
Matheus Clemente <matheusclemente96@hotmail.com>
2019-02-14 21:45:48 -0200

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/matheusclemente/cursomc/services/CategoriaService.java
+++ src/main/java/com/matheusclemente/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.matheusclemente.cursomc.domain.Categoria;
 import com.matheusclemente.cursomc.repositories.CategoriaRepository;
+import com.matheusclemente.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,6 +17,7 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
