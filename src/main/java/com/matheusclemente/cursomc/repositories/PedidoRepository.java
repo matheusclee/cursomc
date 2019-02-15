f22236cdbf813c4941a5f836a41b0fe4365f31af
Matheus Clemente <matheusclemente96@hotmail.com>
2019-02-15 16:43:01 -0200

    Pedido, EstadoPagamamento e Pagamento

---
+++ src/main/java/com/matheusclemente/cursomc/repositories/PedidoRepository.java
@@ -0,0 +1,11 @@
+package com.matheusclemente.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.matheusclemente.cursomc.domain.Pedido;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Integer>{
+
+}
