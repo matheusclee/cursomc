4aa715d60c4327dc2510e34d6582b619cee08372
Matheus Clemente <matheusclemente96@hotmail.com>
2019-02-15 22:21:36 -0200

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/matheusclemente/cursomc/services/PedidoService.java
@@ -0,0 +1,23 @@
+package com.matheusclemente.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.matheusclemente.cursomc.domain.Pedido;
+import com.matheusclemente.cursomc.repositories.PedidoRepository;
+import com.matheusclemente.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+}
